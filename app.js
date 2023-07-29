e61e958d86d8532c512c24281a3d58853fa530b7
cc3sar <programador_mo@ciancoders.dev>
2023-07-28 21:52:56 -0600

    video #6

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const lista = document.getElementById("lista-dinamica");
-const arrayItem = [
-    { nombre: "item 1", descripcion: "descripción 1" },
-    { nombre: "item 2", descripcion: "descripción 2" },
-    { nombre: "item 3", descripcion: "descripción 3" },
-];
+const resultado = document.getElementById("resultado");
+const btnAgregar = document.querySelector(".btn-info");
+const btnDisminuir = document.querySelector(".btn-danger");
+let contador = 0;
 
-const fragment = document.createDocumentFragment();
-const template = document.querySelector("template");
-
-arrayItem.forEach((item) => {
-    template.content.querySelector("b").textContent = item.nombre;
-    template.content.querySelector("span").textContent = item.descripcion;
-    const clone = document.importNode(template.content, true);
-    fragment.appendChild(clone);
+btnAgregar.addEventListener("click", () => {
+    console.log("diste click");
+    contador++;
+    pintarContador();
 });
 
-lista.appendChild(fragment);
+btnDisminuir.addEventListener("click", () => {
+    console.log("diste click");
+    contador--;
+    pintarContador();
+});
+
+pintarContador = () => {
+    resultado.textContent = contador;
+};
