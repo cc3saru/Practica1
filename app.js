c12231ca23366c6d0ea2024650611296bb496964
cc3sar <programador_mo@ciancoders.dev>
2023-07-28 21:28:49 -0600

    video #4

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-// const lista = document.getElementById("lista-dinamica");
-
-// // Creamos el <li>
-// const li = document.createElement("li");
-
-// // Agregamos texto al <li>
-// li.textContent = "Mi <li> dinámico";
-
-// // Finalmente incorporamos al <ul>
-// lista.appendChild(li);
-
-// const lista = document.getElementById("lista-dinamica");
-
-// const arrayItem = ["item 1", "item 2", "item 3"];
-
-// arrayItem.forEach((item) => {
-//   const li = document.createElement("li");
-//   li.textContent = item;
-//   lista.appendChild(li);
-// });
-
 const lista = document.getElementById("lista-dinamica");
 
 const arrayItem = ["item 1", "item 2", "item 3"];
 
+const fragment = document.createDocumentFragment();
+
 arrayItem.forEach((item) => {
-  lista.innerHTML += `
-            <li>${item}</li>
-          `;
+    const li = document.createElement("li");
+    li.textContent = item;
+    fragment.appendChild(li);
 });
+
+lista.appendChild(fragment);
