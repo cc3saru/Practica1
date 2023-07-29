09494d4e55c8e7f0b2adbfb351e7c039b793106f
cc3sar <programador_mo@ciancoders.dev>
2023-07-28 21:10:16 -0600

    video #2

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log(document)
-document.querySelector('h1').innerHTML = 'Texto desde JS'
-console.log(document.querySelector('h1'))
+console.log(document.querySelector('h3'))
+console.log(document.querySelector('.h3-danger'))
+console.log(document.querySelector('#parrafo'))
+console.log(document.getElementById('parrafo'))
+
+console.log(document.querySelectorAll('.h3-danger'))
+console.log(document.querySelectorAll('h3'))
+
+const parrafo = document.querySelector('#parrafo')
+parrafo.textContent = 'texto desde JS'
+parrafo.innerHTML = 'texto desde innerHTML'
