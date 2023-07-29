bd2ccd6df94d98d6870f284c6341dca59878fb54
cc3sar <programador_mo@ciancoders.dev>
2023-07-28 21:42:35 -0600

    video #5

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const lista = document.getElementById("lista-dinamica");
-
-const arrayItem = ["item 1", "item 2", "item 3"];
+const arrayItem = [
+    { nombre: "item 1", descripcion: "descripción 1" },
+    { nombre: "item 2", descripcion: "descripción 2" },
+    { nombre: "item 3", descripcion: "descripción 3" },
+];
 
 const fragment = document.createDocumentFragment();
+const template = document.querySelector("template");
 
 arrayItem.forEach((item) => {
-    const li = document.createElement("li");
-    li.textContent = item;
-    fragment.appendChild(li);
+    template.content.querySelector("b").textContent = item.nombre;
+    template.content.querySelector("span").textContent = item.descripcion;
+    const clone = document.importNode(template.content, true);
+    fragment.appendChild(clone);
 });
 
 lista.appendChild(fragment);
